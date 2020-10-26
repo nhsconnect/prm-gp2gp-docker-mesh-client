751d00cb8f6939177a636936d24328d10432b70f
James Green <jagreen@thoughtworks.com>
2020-10-26 15:26:24 +0000

    PRMT-1236 [DS/JG] - Log to normal mesh log file, for now

--- mesh-container/start-client.sh
+++ mesh-container/start-client.sh
@@ -3,4 +3,4 @@
 envsubst < ${MESH_SCRIPTS_HOME}/config-template.cfg > $MESH_APP_HOME/meshclient.cfg
 
 cd $MESH_APP_HOME
-java -jar meshClient.jar ./meshclient.cfg
+java -jar -Dlog4j.configuration=file:./log4j.xml meshClient.jar ./meshclient.cfg
