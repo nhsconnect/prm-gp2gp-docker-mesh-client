6ce10ee37a169c2231197e59e4437081126ef081
James Green <jagreen@thoughtworks.com>
2020-10-26 15:38:59 +0000

    PRMT-1236 [DS/JG] - Invoke mesh installer at runtime

---
+++ mesh-container/download-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+INSTALLER_URL=https://digital.nhs.uk/binaries/content/assets/website-assets/services/message-exchange-for-social-care-and-health-mesh/mesh-installation-pack-client-6-2-0.rar 
+
+curl ${INSTALLER_URL} --output mesh-installer.rar
+
+unrar mesh-installer.rar
+
+rm mesh-installer.rar
--- mesh-container/start-client.sh
+++ mesh-container/start-client.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+java -jar mesh-6.2.0_20180601-installer-signed.jar << EOF
+1
+${MESH_APP_HOME}
+1
+${MESH_DATA_HOME}
+O
+1
+${MESH_MAILBOX_NAME}
+0
+1
+1
+1
+N
+EOF
+
 envsubst < ${MESH_SCRIPTS_HOME}/config-template.cfg > $MESH_APP_HOME/meshclient.cfg
 
 cd $MESH_APP_HOME
