8d3d2ec90c9558e3d5854d9a48154e5929b20f3d
James Green <jagreen@thoughtworks.com>
2020-11-17 17:07:09 +0000

    PMRT-1349 [JG/HZ] Fail fast if required environment variables are not set

--- mesh-container/download-installer.sh
+++ mesh-container/download-installer.sh
@@ -3,6 +3,8 @@ set -e
 
 INSTALLER_URL=https://digital.nhs.uk/binaries/content/assets/website-assets/services/message-exchange-for-social-care-and-health-mesh/mesh-installation-pack-client-6-2-0.rar 
 
+cd ${MESH_SCRIPTS_HOME}
+
 curl ${INSTALLER_URL} --output mesh-installer.rar
 
 unrar mesh-installer.rar
--- mesh-container/start-client.sh
+++ mesh-container/start-client.sh
@@ -2,7 +2,22 @@
 
 set -e
 
-java -jar mesh-6.2.0_20180601-installer-signed.jar << EOF
+if [[ -z "$KEYSTORE_PASSWORD" ]]; then
+    echo "Error: KEYSTORE_PASSWORD environment variable is not set, exiting.."
+    exit 1
+fi
+
+if [[ -z "$MESH_MAILBOX_NAME" ]]; then
+    echo "Error: MESH_MAILBOX_NAME environment variable is not set, exiting.."
+    exit 1
+fi
+
+if [[ -z "$MESH_MAILBOX_PASSWORD" ]]; then
+    echo "Error: MESH_MAILBOX_PASSWORD environment variable is not set, exiting.."
+    exit 1
+fi
+
+java -jar ${MESH_SCRIPTS_HOME}/mesh-6.2.0_20180601-installer-signed.jar << EOF
 1
 ${MESH_APP_HOME}
 1
@@ -16,6 +31,7 @@ ${MESH_MAILBOX_NAME}
 1
 N
 EOF
+
 
 envsubst < ${MESH_SCRIPTS_HOME}/config-template.cfg > ${MESH_APP_HOME}/meshclient.cfg
 cp ${MESH_SCRIPTS_HOME}/log4j.xml ${MESH_APP_HOME}/log4j.xml
